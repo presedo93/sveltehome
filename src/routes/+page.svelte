1c790d572a0d71b34f7a2a9fbd1341bc9c16cd8b
Rene Presedo <presedo.93@gmail.com>
2023-12-15 20:22:09 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { createCollapsible, melt } from '@melt-ui/svelte';
+	const {
+		elements: { root, content, trigger },
+		states: { open }
+	} = createCollapsible();
+</script>
+
+<h1 class="font-bold">Welcome to SvelteKit</h1>
+<div use:melt={$root}>
+	<button use:melt={$trigger}>{$open ? 'Close' : 'Open'}</button>
+	<div use:melt={$content}>Obi-Wan says: Hello there!</div>
+</div>
