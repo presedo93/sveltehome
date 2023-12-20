b3d58bc35bc98147b824566f3b4f5d81ab218896
Rene Presedo <presedo.93@gmail.com>
2023-12-20 17:43:10 +0100

    Add home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,8 @@
 	<HelloWorld />
 	<div class="mx-6 rounded p-4 md:px-12">
 		<p class="mb-8 text-start">
-			I'm <strong>Rene Presedo!</strong> And this is the place where you can know about me, so make yourself
-			at home!
+			Hi! I'm <strong>Rene Presedo!</strong> And this is the place where you can know about me, so make
+			yourself at home!
 		</p>
 		<p class="mb-8 text-start">
 			I'm a software developer placed in Malaga, in the south of Spain. I started my working career
@@ -15,12 +15,17 @@
 			But before getting down to business, here it is a little bit about me!
 		</p>
 		<p class="mb-8 text-start">
-			I was also born in 1993 in Malaga, and I've spent my whole life in Fuengirola (a small town
+			I was born in 1993 also in Malaga, and I've spent my whole life in Fuengirola (a small town
 			30km away from it)! Since I was a child, I've been always interested in technology. The first
 			computer that we had in my parents home was an Intel Pentium II running Windows 98. I could
 			say that I spent all my childhood learning coding stuff but it would be a lie... I was playing
-			games like <code class="text-yellow-300">Jazz Jackrabbit</code>
+			games like <code class="text-yellow-300">Jazz Jackrabbit</code>!
 		</p>
+		<p class="mb-8 text-start">
+			Nowadays, my hobbies are sport related, some video gaming when I have some free time and do
+			some more coding!
+		</p>
+		<p></p>
 		<div class="mb-8 flex w-full items-center justify-center">
 			<div class="h-36 w-36 overflow-hidden rounded-full border border-white">
 				<img alt="Profile" src={'/images/presedo.jpg'} />
