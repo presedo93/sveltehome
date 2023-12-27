9d1f08cab4d2ce8d456383a85eb647154595c786
Rene Presedo <presedo.93@gmail.com>
2023-12-27 20:31:56 +0100

    Improved snake game

--- src/lib/components/snake.svelte
+++ src/lib/components/snake.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
-	import init from 'snake';
+	import init, { launch, reset } from 'snake';
 	import { onMount } from 'svelte';
+
+	let launched = false;
 
 	onMount(() => {
 		init();
 	});
+
+	const launchGame = () => {
+		launch();
+		launched = true;
+	};
+
+	const resetGame = () => {
+		reset();
+	};
 </script>
 
-<div id="root" class="h-[250px] w-[250px] rounded-md bg-gray-100 p-2 dark:bg-gray-900" />
+<div class="flex flex-col items-center">
+	<div id="root" class="rounded-md bg-gray-100 p-2 dark:bg-gray-900" />
+	{#if launched}
+		<button class="mt-2 rounded-md bg-red-800 px-3 text-white" on:click={resetGame}>Reset</button>
+	{:else}
+		<button class="mt-2 rounded-md bg-red-800 px-3 text-white" on:click={launchGame}>Launch</button>
+	{/if}
+</div>
