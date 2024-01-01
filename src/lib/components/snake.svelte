840aa3cc52a96fb6860a75c51b20a5b367faf0d7
Rene Presedo <presedo.93@gmail.com>
2024-01-01 20:46:36 +0100

    Mobile buttons for Snake game

--- src/lib/components/snake.svelte
+++ src/lib/components/snake.svelte
@@ -2,6 +2,13 @@
 	import init, { startSnake, resetSnake, renderBoard } from 'snake';
 	import { onMount } from 'svelte';
 
+	enum Direction {
+		Up = 1,
+		Down,
+		Left,
+		Right
+	}
+
 	let launched = false;
 
 	onMount(async () => {
@@ -17,6 +24,26 @@
 			launched = true;
 		}
 	};
+
+	const handleDir = (dir: Direction) => {
+		let key = '0';
+		switch (dir) {
+			case Direction.Up:
+				key = 'w';
+				break;
+			case Direction.Down:
+				key = 's';
+				break;
+			case Direction.Left:
+				key = 'a';
+				break;
+			case Direction.Right:
+				key = 'd';
+				break;
+		}
+
+		window.dispatchEvent(new KeyboardEvent('keydown', { key }));
+	};
 </script>
 
 <div class="flex flex-col items-center">
@@ -24,4 +51,24 @@
 	<button class="mt-4 rounded-md bg-red-400 px-3" on:click={handleButton}
 		>{launched ? 'Reset the game' : 'Start the game'}</button
 	>
+	<div class=" mt-6 flex flex-col md:hidden">
+		<button
+			class="mb-2 self-center rounded-md bg-orange-400 px-3 py-1"
+			on:click={() => handleDir(Direction.Up)}>W</button
+		>
+		<div>
+			<button
+				class="w-fit rounded-md bg-orange-400 px-3 py-1"
+				on:click={() => handleDir(Direction.Left)}>A</button
+			>
+			<button
+				class="w-fit rounded-md bg-orange-400 px-3 py-1"
+				on:click={() => handleDir(Direction.Down)}>S</button
+			>
+			<button
+				class="w-fit rounded-md bg-orange-400 px-3 py-1"
+				on:click={() => handleDir(Direction.Right)}>D</button
+			>
+		</div>
+	</div>
 </div>
